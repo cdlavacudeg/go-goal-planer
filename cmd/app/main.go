77e0f873c9cfa4d9a6b085604797fca245b8efea
cdlavacudeg <cristianlav10@gmail.com>
2023-11-06 10:43:04 -0500

    [Add] Aws session and dynamoDB setup

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
@@ -20,6 +24,50 @@ import (
 // @in header
 // @name Authorization
 func main() {
+	awsSession, err := setupAwsSession()
+	if err != nil {
+		panic(err)
+	}
+
+	svc := dynamodb.New(awsSession)
+	// Create table Movies
+	tableName := "Users"
+
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Name"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Email"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Name"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("Email"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.CreateTable(input)
+	if err != nil {
+		fmt.Printf("Got error calling CreateTable: %s", err)
+	}
+
+	fmt.Println("Created the table", tableName)
+
 	r := gin.Default()
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	v1 := r.Group("/api/v1")
@@ -49,4 +97,17 @@ func main() {
 // @Router /example/helloworld [get]
 func Helloworld(g *gin.Context) {
 	g.JSON(http.StatusOK, "Hello World!")
+}
+
+func setupAwsSession() (*session.Session, error) {
+	// region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region:   aws.String("localhost"),
+		Endpoint: aws.String("http://localhost:8000"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return awsSession, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cdlavacudeg/go-goal-planner
 go 1.21.3
 
 require (
+	github.com/aws/aws-sdk-go v1.47.3
 	github.com/gin-gonic/gin v1.9.1
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
@@ -25,6 +26,7 @@ require (
 	github.com/go-playground/validator/v10 v10.15.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
